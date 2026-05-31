182a1d08dd5e4602b5efd6471d85905b2fea0dd3
HarshAmble <ambleharsh@gmail.com>
2026-05-31 14:18:34 +0530

    Added validation using the validation dependency which will validate the input param for the createUser method in the UserController

--- src/main/java/com/fridgeapp/dto/UserCreateRequest.java
+++ src/main/java/com/fridgeapp/dto/UserCreateRequest.java
@@ -1,5 +1,7 @@
 package com.fridgeapp.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +10,8 @@ import lombok.Setter;
 public class UserCreateRequest {
     private String username;
     private String password;
+
+    @NotBlank(message = "Email address is required.")
+    @Email(message = "Invalid email address format.")
     private String email;
 }
